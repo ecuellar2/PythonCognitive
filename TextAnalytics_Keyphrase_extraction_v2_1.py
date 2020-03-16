24ddbb8436ff41113d9a9b32c4fa6016a664845b
Edgar <ecuellar2@yahoo.com>
2020-03-15 19:27:47 -0500

    Create TextAnalytics_Keyphrase_extraction_v2_1.py

---
+++ TextAnalytics_Keyphrase_extraction_v2_1.py
@@ -0,0 +1,49 @@
+print("starting ")
+#Know location such as  C:\Users\xxx\AppData\Local\Programs\Python\Python38-32
+#python.exe -m pip install azure-cognitiveservices-language-textanalytics
+
+subscription_key = ""
+endpoint = ""
+
+import os
+from azure.cognitiveservices.language.textanalytics import TextAnalyticsClient
+from msrest.authentication import CognitiveServicesCredentials
+
+def authenticateClient():
+    credentials = CognitiveServicesCredentials(subscription_key)
+    text_analytics_client = TextAnalyticsClient(
+        endpoint=endpoint, credentials=credentials)
+    return text_analytics_client
+
+def key_phrases():
+    
+    client = authenticateClient()
+
+    try:
+        documents = [
+            {"id": "1", "language": "ja", "text": "猫は幸せ"},
+            {"id": "2", "language": "de",
+                "text": "Fahrt nach Stuttgart und dann zum Hotel zu Fu."},
+            {"id": "3", "language": "en",
+                "text": "My cat might need to see a veterinarian."},
+            {"id": "4", "language": "es", "text": "A mi me encanta el fútbol!"}
+        ]
+
+        for document in documents:
+            print(
+                "Asking key-phrases on '{}' (id: {})".format(document['text'], document['id']))
+
+        response = client.key_phrases(documents=documents)
+
+        for document in response.documents:
+            print("Document Id: ", document.id)
+            print("\tKey Phrases:")
+            for phrase in document.key_phrases:
+                print("\t\t", phrase)
+
+    except Exception as err:
+        print("Encountered exception. {}".format(err))
+        
+key_phrases()
+
+print("All done")
