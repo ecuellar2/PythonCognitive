86bc1502118191b2e7fcb9d8999bec07a6b7a17e
Edgar <ecuellar2@yahoo.com>
2020-03-22 19:54:48 -0500

    Create TranslatorText_lookup_word_translation_v3.py

---
+++ TranslatorText/TranslatorText_lookup_word_translation_v3.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+#C:\Python\Python38-32\python.exe -m pip install requests uuid
+
+import os, requests, uuid, json
+key = ""
+endpoint = "https://api.cognitive.microsofttranslator.com/dictionary/lookup?api-version=3.0&from=en&to=es"
+
+
+
+
+print("Starting")
+
+headers = {
+    'Ocp-Apim-Subscription-Key': key,
+    'Ocp-Apim-Subscription-Region': 'southcentralus',
+    'Content-type': 'application/json',
+    'X-ClientTraceId': str(uuid.uuid4())
+}
+
+body = [{
+    'text': 'Elephants'
+}]
+
+request = requests.post(endpoint, headers=headers, json=body)
+response = request.json()
+
+print(json.dumps(response, sort_keys=True, indent=4,
+                 ensure_ascii=False, separators=(',', ': ')))
+
+print("All done")
