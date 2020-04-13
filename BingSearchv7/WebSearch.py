da0536a53cbda79f7989c1e8b44039beeb5c756b
Edgar <ecuellar2@yahoo.com>
2020-04-12 21:47:21 -0500

    Update WebSearch.py

--- BingSearchv7/WebSearch.py
+++ BingSearchv7/WebSearch.py
@@ -18,6 +18,15 @@ endpoint = "https://southcentralus.api.cognitive.microsoft.com/bing/v7.0/search?
 
 client = WebSearchClient(endpoint, CognitiveServicesCredentials(key))
 web_data = client.web.search(query="Yosemite")
+
+
+print("sent search query ")
+'''
+Web pages
+If the search response contains web pages, the first result's name and url
+are printed.
+'''
+
 if hasattr(web_data.web_pages, 'value'):
 
     print("\r\nWebpage Results#{}".format(len(web_data.web_pages.value)))
@@ -28,5 +37,53 @@ if hasattr(web_data.web_pages, 'value'):
 
 else:
     print("Didn't find any web pages...")
+
+
+'''
+Images
+If the search response contains images, the first result's name and url
+are printed.
+'''
+if hasattr(web_data.images, 'value'):
+
+    print("\r\nImage Results#{}".format(len(web_data.images.value)))
+
+    first_image = web_data.images.value[0]
+    print("First Image name: {} ".format(first_image.name))
+    print("First Image URL: {} ".format(first_image.url))
+
+else:
+    print("Didn't find any images...")
+
+'''
+News
+If the search response contains news, the first result's name and url
+are printed.
+'''
+if hasattr(web_data.news, 'value'):
+
+    print("\r\nNews Results#{}".format(len(web_data.news.value)))
+
+    first_news = web_data.news.value[0]
+    print("First News name: {} ".format(first_news.name))
+    print("First News URL: {} ".format(first_news.url))
+
+else:
+    print("Didn't find any news...")
+
+'''
+If the search response contains videos, the first result's name and url
+are printed.
+'''
+if hasattr(web_data.videos, 'value'):
+
+    print("\r\nVideos Results#{}".format(len(web_data.videos.value)))
+
+    first_video = web_data.videos.value[0]
+    print("First Videos name: {} ".format(first_video.name))
+    print("First Videos URL: {} ".format(first_video.url))
+
+else:
+    print("Didn't find any videos...")
 
 print("done")
