8f2ceb89580c2206f28b7f1f0786e9302ddf5f5a
Edgar <ecuellar2@yahoo.com>
2020-06-07 20:10:01 -0500

    Create q_n_a_Update_publish_downloadKB.py

---
+++ QnA/q_n_a_Update_publish_downloadKB.py
@@ -0,0 +1,48 @@
+# python -m pip install azure-cognitiveservices-knowledge-qnamaker
+# https://docs.microsoft.com/en-us/azure/cognitive-services/qnamaker/quickstarts/quickstart-sdk?pivots=programming-language-python
+# https://docs.microsoft.com/en-us/python/api/azure-cognitiveservices-knowledge-qnamaker/azure.cognitiveservices.knowledge.qnamaker?view=azure-python
+# https://www.qnamaker.ai/
+
+import os
+import time
+
+#0.2.0 (2020-03-17)  The QnAMakerClient client moved from knowledge.qnamaker to knowledge.qnamaker.authoring
+from azure.cognitiveservices.knowledge.qnamaker.authoring import QnAMakerClient
+from azure.cognitiveservices.knowledge.qnamaker.runtime.models import MetadataDTO, QnADTO, QnADTOContext, PromptDTOQna, PromptDTO, ContextDTO
+from azure.cognitiveservices.knowledge.qnamaker.runtime.models import QueryDTOContext, QueryDTO, QueryContextDTO, QnASearchResultContext, QnASearchResult
+from azure.cognitiveservices.knowledge.qnamaker.runtime.models import QnASearchResultList, FeedbackRecordDTO, FeedbackRecordsDTO
+from azure.cognitiveservices.knowledge.qnamaker.authoring.models import OperationStateType
+from azure.cognitiveservices.knowledge.qnamaker.authoring.models import CreateKbDTO, UpdateKbOperationDTO, UpdateKbOperationDTOAdd
+from msrest.authentication import CognitiveServicesCredentials
+
+print("starting ")
+
+key = ""
+#endpoint_string = "https://q-n-a.cognitiveservices.azure.com/qnamaker/v4.0" #  westus
+#endpoint_string = https://westus.api.cognitive.microsoft.com
+endpoint_string = "https://q-n-a.cognitiveservices.azure.com"
+
+client = QnAMakerClient(endpoint=endpoint_string, credentials=CognitiveServicesCredentials(key))
+#create_op = client.knowledgebase.create(create_kb_payload=create_kb_dto)  
+#operation = client.operations.get_details(operation_id=operation_id)
+kb_id = ""
+
+update_kb_operation_dto = UpdateKbOperationDTO(add=UpdateKbOperationDTOAdd(
+    qna_list=[
+                QnADTO(questions=["bye"], answer="goodbye")
+            ]
+        )
+    )
+
+#update_op = client.knowledgebase.update(kb_id=kb_id, update_kb=update_kb_operation_dto)
+#print (update_op.operation_id)
+
+
+#client.knowledgebase.publish(kb_id=kb_id)
+#kb_data = client.knowledgebase.download(kb_id=kb_id, environment="Prod")
+#print("KB Downloaded. It has {} QnAs.".format(len(kb_data.qna_documents)))
+#client.knowledgebase.delete(kb_id=kb_id)
+
+
+
+print("Done ")
