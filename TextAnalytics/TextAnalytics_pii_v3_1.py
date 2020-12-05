7234047c36c0560147e4b9030ff919c7c3a624aa
Edgar <ecuellar2@yahoo.com>
2020-12-04 23:29:20 -0600

    Create TextAnalytics_pii_v3_1.py

---
+++ TextAnalytics/TextAnalytics_pii_v3_1.py
@@ -0,0 +1,37 @@
+print("starting ")
+# Know location such as  C:\Users\xxx\AppData\Local\Programs\Python\Python38-32
+# C:\python\python.exe -m pip install azure-ai-textanalytics --pre
+
+key = "xx"
+endpoint = "https://x.cognitiveservices.azure.com/"
+
+from azure.ai.textanalytics import TextAnalyticsClient
+from azure.core.credentials import AzureKeyCredential
+
+def authenticate_client():
+    ta_credential = AzureKeyCredential(key)
+    text_analytics_client = TextAnalyticsClient(
+            endpoint=endpoint, 
+            credential=ta_credential)
+    return text_analytics_client
+
+client = authenticate_client()
+
+def pii_recognition_example(client):
+    documents = [
+        "The employee's SSN is 859-98-0987.",
+        "The employee's phone number is 555-555-5555."
+    ]
+    response = client.recognize_pii_entities(documents, language="en")
+    result = [doc for doc in response if not doc.is_error]
+    for doc in result:
+        print("Redacted Text: {}".format(doc.redacted_text))
+        for entity in doc.entities:
+            print("Entity: {}".format(entity.text))
+            print("\tCategory: {}".format(entity.category))
+            print("\tConfidence Score: {}".format(entity.confidence_score))
+            print("\tOffset: {}".format(entity.offset))
+pii_recognition_example(client)
+
+
+print("All done")
